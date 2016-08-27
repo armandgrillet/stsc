8324dc34a02b8d7b389f1aa08e66612703ed3e01
Armand Grillet <ArmandGrillet@users.noreply.github.com>
2016-08-27 15:19:33 +0200

    Timer added.

--- src/main/scala/stsc/Job.scala
+++ src/main/scala/stsc/Job.scala
@@ -5,6 +5,7 @@ import org.apache.spark.SparkConf
 
 object Job {
     def main(args: Array[String]) {
+        val t0 = System.nanoTime()
         val conf = new SparkConf().setAppName("STSCSparkJob")
         val sc = new SparkContext(conf)
         val dataset = getClass.getResource("/dataset.csv").getPath()
@@ -12,6 +13,8 @@ object Job {
         sc.addJar("/Users/Armand/Code/stsc/target/scala-2.11/stsc-assembly-1.0.jar")
         // STSC.sparkCluster(sc, dataset, kdtree, "clusters")
         STSC.sparkCluster(sc, dataset, kdtree, "clusterCenters.csv", true, 2, 12)
+        val t1 = System.nanoTime()
+        println("Elapsed time: " + (t1 - t0) + "ns")
         sc.stop()
 
         // val dataPath = getClass.getResource("/dataset.csv").getPath()
